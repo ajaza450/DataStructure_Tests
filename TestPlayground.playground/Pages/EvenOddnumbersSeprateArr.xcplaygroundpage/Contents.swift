7aa65d00e1df6a5418dffa0a9d4b60ff49dc5891
Ezaz Ahmad <ajaza450@gmail.com>
2023-10-18 15:21:27 +0530

    commti code

--- TestPlayground.playground/Pages/EvenOddnumbersSeprateArr.xcplaygroundpage/Contents.swift
+++ TestPlayground.playground/Pages/EvenOddnumbersSeprateArr.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 
-var array = [1,24,2,3,4,5]
+var array = [1,24,2,3,5,5]
 
 
 
@@ -25,20 +25,20 @@ func arrangeEvenOdd( arr: inout [Int]) {
        // print(" Move right pointer to first even number from right")
         // Move right pointer to first even number from right
         while arr[right] % 2 != 0 && left < right {
-      //      print("step 3 : ",left,"right",right,"arr[right]",arr[right])
+            print("step 3 : ",left,"right",right,"arr[right]",arr[right])
             right -= 1
         }
         
         // Swap the two elements
         if left < right {
-          //  print("step 4 : ",left,"right",right,"arr",arr)
+            print("step 4 : ",left,"right",right,"arr",arr)
             let temp = arr[left]
             arr[left] = arr[right]
             arr[right] = temp
             
             left += 1
             right -= 1
-      //      print("step 5 : ",left,"right",right,"arr",arr)
+            print("step 5 : ",left,"right",right,"arr",arr)
         }
     }
 }
@@ -47,11 +47,3 @@ arrangeEvenOdd(arr: &array)
 print(array) // Output: [4, 2, 3, 1, 5]
 
 
-var left = 0
-var right = arr.count - 1
-
-
-let occurrences = array.reduce(into: [], { partialResult, number in
-    partialResult[number,default : 0] += 1
-})
-print("occurrences",occurrences)
--- TestPlayground.playground/Pages/Fine numbers of duplicate element.xcplaygroundpage/Contents.swift
+++ TestPlayground.playground/Pages/Fine numbers of duplicate element.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 
-
+//Fine numbers of duplicate element
 
 let array = [2,4,3,3,5,2]
 
@@ -35,3 +35,24 @@ print("occurrences",occurrences)
 for (number, count) in occurrences {
     print("Number \(number) appears \(count) times")
 }
+
+////String
+
+let str = "AAABBBCCDD"
+var temp = [Character:Int]()
+
+for char in str {
+    temp[char, default: 0] += 1
+
+}
+print( temp)
+for (char ,count) in temp {
+    print(char, "=", count)
+}
+
+
+
+var data = str.reduce(into: [Character: Int]()) { partialResult, char in
+    partialResult[char, default: 0] += 1
+}
+print(data)
